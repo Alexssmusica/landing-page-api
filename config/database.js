1ae2e86b42f3b7d7edf16d4909e52acf6473f7a1
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-01 20:17:38 -0300

    ajustes

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
-        port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
-        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
-        username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
-        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-        ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'jclan'),
+        password: env('DATABASE_PASSWORD', 'jcuser'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
     },
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', process.env.HOST),
-  port: env.int('PORT', process.env.PORT),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd09a9b34c99b8ff50b72e8f74b027f30'),
