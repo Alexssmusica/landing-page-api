ee7f8b5fdfe1380c0d798efd0b19471360b3fba2
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-01 20:10:40 -0300

    ajustes

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', process.env.PORT),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
+      secret: env('ADMIN_JWT_SECRET', 'd09a9b34c99b8ff50b72e8f74b027f30'),
     },
   },
 });
